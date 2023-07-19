e4be8d54bb4e13cd905270c1d9c9781dc00181f2
Eric Afes <eriicafes@gmail.com>
2023-07-19 09:24:48 +0100

    Remove preserveSymlinks

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    resolve: {
-        preserveSymlinks: true,
-    },
     test: {
         environment: "miniflare",
         environmentOptions: {},
