c34a353db2a0d2ccbad978625730d569d1cb31c4
Eric Afes <eriicafes@gmail.com>
2023-07-19 08:12:10 +0100

    Test with supertest

--- tests/setup.js
+++
@@ -1,6 +0,0 @@
-import nodeFetch from 'node-fetch'
-// Mocking fetch Web API using node-fetch
-if (typeof fetch === 'undefined') {
-    global.fetch = nodeFetch
-    global.Request = nodeFetch.Request
-}
